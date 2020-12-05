b67414119ff701f3921e05ca17f9d201b108f7ef
muslima <mus@mus.mus>
2020-12-05 17:18:54 +0500

    add width,height

--- js/myJQuery.js
+++ js/myJQuery.js
@@ -84,7 +84,7 @@ function jQ(params) {
             } else if (typeof newWidth === "string") {
                 this.element[i].style.width = newWidth
             } else  {
-                this.element[i].style.width = this.element[0].clientWidth + "px"
+                return this.element[0].clientWidth + "px"
             }
         }
         return this
@@ -96,7 +96,7 @@ function jQ(params) {
             } else if (typeof newHeight === "string") {
                 this.element[i].style.height = newHeight
             } else {
-                this.element[i].style.height = this.element[0].clientHeight + "px"
+               return this.element[0].clientHeight + "px"
             }
         }
         return this
